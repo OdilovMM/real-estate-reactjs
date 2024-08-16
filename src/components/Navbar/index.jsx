884ed92ab4cc35f967ccd5706fa0345f8dfc14ff
OdilovMM <dssrinman@gmail.com>
2024-08-16 21:08:15 +0500

    add: navbar finished after configuration setup

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { Container, Link, Logo, Section, Wrapper, Main } from "./style";
+import { navbar } from "../../utils/navbar";
+
+export const Home = () => {
+  const navigate = useNavigate();
+  return (
+    <Container>
+      <Main>
+        <Wrapper>
+          <Section onClick={() => navigate("/home")} logo>
+            <Logo /> <h3>REsTate</h3>
+          </Section>
+          <Section>
+            {navbar.map(({ title, path }, index) => {
+              return (
+                <Link
+                  className={({ isActive }) => isActive && "active"}
+                  key={index}
+                  to={path}
+                >
+                  {title}
+                </Link>
+              );
+            })}
+          </Section>
+          <Section>
+            <button>Sign in</button>
+          </Section>
+        </Wrapper>
+      </Main>
+      <Outlet />
+    </Container>
+  );
+};
+
+export default Home;
